e15e7cba2e68d5255e02c9b3daca112d4b2622df
felipeog <felipeog476@gmail.com>
2019-10-02 02:45:35 -0300

    Adiciona tokens de acesso

--- src/config/Snoowrap.js
+++ src/config/Snoowrap.js
@@ -1,11 +1,10 @@
 import Snoowrap from "snoowrap";
 
 const r = new Snoowrap({
-  userAgent: process.env.REACT_APP_USER_AGENT,
-  clientId: process.env.REACT_APP_CLIENT_ID,
-  clientSecret: process.env.REACT_APP_CLIENT_SECRET,
-  username: process.env.REACT_APP_REDDIT_USER,
-  password: process.env.REACT_APP_REDDIT_PASS
+  userAgent: "winnin-desafio-frontend",
+  clientId: "WikUjYybBABEhQ",
+  refreshToken: "135720659019-PfdjvJYLwtP5rIzbdiYC0VfIgFc",
+  accessToken: "135720659019-XW0GC0bqaImr261cS6aEsFb2KDY"
 });
 
 export default r;
