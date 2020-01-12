3fabebd984c0d65e79e684094659df4bafdcdd26
unknown <you@example.com>
2020-01-12 01:34:47 -0300

    Update routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
-} from "react-router-dom";
+  Redirect,
+} from 'react-router-dom'
 
-import Header from "../Header/Header";
-import HotFeed from "../HotFeed/HotFeed";
-import NewFeed from "../NewFeed/NewFeed";
-import RisingFeed from "../RisingFeed/RisingFeed";
+import Header from '../Header/Header'
+import HotFeed from '../HotFeed/HotFeed'
+import NewFeed from '../NewFeed/NewFeed'
+import RisingFeed from '../RisingFeed/RisingFeed'
 
 class App extends Component {
   render() {
@@ -18,25 +18,13 @@ class App extends Component {
         <Header />
 
         <Switch>
-          <Route exact path="/">
-            <Redirect to="/hot" />
-          </Route>
-
-          <Route path="/hot">
-            <HotFeed />
-          </Route>
-
-          <Route path="/new">
-            <NewFeed />
-          </Route>
-
-          <Route path="/rising">
-            <RisingFeed />
-          </Route>
+          <Route exact path={['/', '/hot']} component={HotFeed} />
+          <Route path="/new" component={NewFeed} />
+          <Route path="/rising" component={RisingFeed} />
         </Switch>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App
