e3b99bc2c62f3cb9f93880dc35554f524d3084eb
unknown <you@example.com>
2020-01-12 03:33:15 -0300

    Install Moment JS

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,29 +1,14 @@
 import React from 'react'
+import moment from 'moment'
+import 'moment/locale/pt-br'
 import './index.scss'
-
-const formatTime = (utc) => {
-  const now = new Date()
-  const created = new Date(utc * 1000)
-  const timeDifference = Math.abs(now - created)
-  let formattedTime = ''
-
-  if (timeDifference / (1000 * 60 * 60) < 1) {
-    formattedTime += Math.floor(timeDifference / (1000 * 60)) + ' minutos'
-  } else if (Math.floor(timeDifference / (1000 * 60 * 60)) === 1) {
-    formattedTime += '1 hora'
-  } else {
-    formattedTime += Math.floor(timeDifference / (1000 * 60 * 60)) + ' horas'
-  }
-
-  return formattedTime
-}
 
 const formatData = (post) => {
   const thumb = post.thumbnail.match('http') ? post.thumbnail : false
   const title = post.title
   const user = post.author.name
   const url = 'https://www.reddit.com' + post.permalink
-  const time = formatTime(post.created_utc)
+  const time = moment.unix(post.created_utc).fromNow()
 
   return { thumb, title, user, url, time }
 }
@@ -48,7 +33,7 @@ const PostItem = ({ post }) => {
         <div className="post-item__content">
           <h1 className="post-item__title">{title}</h1>
           <p className="post-item__meta">
-            <span className="post-item__time">Enviado há {time}</span> por{' '}
+            <span className="post-item__time">Enviado {time}</span> por{' '}
             <span className="post-item__user">{user}</span>
           </p>
           <p className="post-item__url">{url}</p>
