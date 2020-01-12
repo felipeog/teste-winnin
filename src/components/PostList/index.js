a7ee9283371cda010b4447f24e4ec57695998a8b
felipeog <felipeog476@gmail.com>
2020-01-12 16:45:13 -0300

    Use Hooks

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,121 +1,135 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.scss'
 import { PostItem } from 'components'
 import r from 'config/Snoowrap'
 
-class PostList extends Component {
-  state = {
-    after: null,
-    board: this.props.subreddit,
-    end: false,
-    error: false,
-    loading: true,
-    posts: [],
-  }
+const PostList = ({ subreddit }) => {
+  const [after, setAfter] = useState(null)
+  const [end, setEnd] = useState(false)
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [posts, setPosts] = useState([])
+  const options = { limit: 10 }
 
-  options = { limit: 10 }
+  useEffect(() => {
+    loadPosts()
+  }, [])
 
-  componentDidMount() {
-    this.loadPosts()
-  }
+  const loadPosts = async () => {
+    setLoading(true)
+    const board = await r.getSubreddit('reactjs')
 
-  loadPosts = async () => {
-    try {
-      this.setState({ loading: true })
+    switch (subreddit) {
+      case 'hot':
+        try {
+          let loadedPosts = await board.getHot({
+            ...options,
+            after,
+          })
 
-      const board = await r.getSubreddit('reactjs')
-      let posts,
-        after,
-        end = false
-
-      switch (this.props.subreddit) {
-        case 'hot':
           let stickied = 0
+          loadedPosts.forEach((newPost) => newPost.stickied && stickied++)
 
-          posts = await board.getHot({
-            ...this.options,
-            after: this.state.after,
+          if (stickied)
+            loadedPosts = loadedPosts.slice(0, loadedPosts.length - stickied)
+
+          setPosts([...posts, ...loadedPosts])
+
+          if (!loadedPosts.length || loadedPosts.length < options.limit) {
+            setAfter(null)
+            setEnd(true)
+          } else {
+            setAfter(loadedPosts[loadedPosts.length - 1].name)
+          }
+        } catch (e) {
+          console.error('PostList@loadPosts hot >>>>>>', e)
+          setError(true)
+        } finally {
+          setLoading(false)
+        }
+        break
+
+      case 'new':
+        try {
+          const loadedPosts = await board.getNew({
+            ...options,
+            after,
           })
 
-          posts.forEach((post) => {
-            if (post.stickied) stickied++
+          setPosts([...posts, ...loadedPosts])
+
+          if (!loadedPosts.length || loadedPosts.length < options.limit) {
+            setAfter(null)
+            setEnd(true)
+          } else {
+            setAfter(loadedPosts[loadedPosts.length - 1].name)
+          }
+        } catch (e) {
+          console.error('PostList@loadPosts new >>>>>>', e)
+          setError(true)
+        } finally {
+          setLoading(false)
+        }
+        break
+
+      case 'rising':
+        try {
+          const loadedPosts = await board.getRising({
+            ...options,
+            after,
           })
 
-          if (stickied) posts = posts.slice(0, posts.length - stickied)
-          break
+          setPosts([...posts, ...loadedPosts])
 
-        case 'new':
-          posts = await board.getNew({
-            ...this.options,
-            after: this.state.after,
-          })
-          break
+          if (!loadedPosts.length || loadedPosts.length < options.limit) {
+            setAfter(null)
+            setEnd(true)
+          } else {
+            setAfter(loadedPosts[loadedPosts.length - 1].name)
+          }
+        } catch (e) {
+          console.error('PostList@loadPosts rising >>>>>>', e)
+          setError(true)
+        } finally {
+          setLoading(false)
+        }
+        break
 
-        case 'rising':
-          posts = await board.getRising({
-            ...this.options,
-            after: this.state.after,
-          })
-          break
-
-        default:
-          break
-      }
-
-      if (!posts.length || posts.length < this.options.limit) {
-        after = null
-        end = true
-      } else {
-        after = posts[posts.length - 1].name
-      }
-
-      this.setState({
-        posts: [...this.state.posts, ...posts],
-        after,
-        end,
-        loading: false,
-      })
-    } catch (err) {
-      console.error(err)
-
-      this.setState({ error: true })
+      default:
+        break
     }
   }
 
-  render() {
-    const { posts, end, error, loading } = this.state
+  return (
+    <React.Fragment>
+      {error ? (
+        <section className="error">
+          <p className="error__message">Erro ao carregar posts</p>
+        </section>
+      ) : (
+        <section className="post-list">
+          {posts.length ? (
+            posts.map((post) => <PostItem key={post.id} post={post} />)
+          ) : (
+            <p className="post-list__loading">Carregando posts...</p>
+          )}
 
-    return (
-      <React.Fragment>
-        {error ? (
-          <section className="error">
-            <p className="error__message">Erro ao carregar posts</p>
-          </section>
-        ) : (
-          <section className="post-list">
-            {posts.length ? (
-              posts.map((post) => <PostItem key={post.id} post={post} />)
-            ) : (
-              <p className="post-list__loading">Carregando posts...</p>
-            )}
+          {end ? <p className="post-list__end">Fim do conteúdo</p> : ''}
 
-            {end ? <p className="post-list__end">Fim do conteúdo</p> : ''}
-
-            <button
-              type="button"
-              disabled={end || loading ? 'disabled' : ''}
-              className={`button load-more ${
-                end || loading ? 'button--disabled' : ''
-              }`}
-              onClick={this.loadPosts}
-            >
-              + Ver mais
-            </button>
-          </section>
-        )}
-      </React.Fragment>
-    )
-  }
+          <button
+            type="button"
+            disabled={end || loading ? 'disabled' : ''}
+            className={`button load-more ${
+              end || loading ? 'button--disabled' : ''
+            }`}
+            onClick={loadPosts}
+          >
+            + Ver mais
+          </button>
+        </section>
+      )}
+    </React.Fragment>
+  )
 }
 
 export default PostList
