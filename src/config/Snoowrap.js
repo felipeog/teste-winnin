9400f3961866ce4a14001d9253357a8b2ac2c34f
unknown <you@example.com>
2020-01-12 00:44:52 -0300

    Add environment variables

--- src/config/Snoowrap.js
+++ src/config/Snoowrap.js
@@ -1,10 +1,10 @@
-import Snoowrap from "snoowrap";
+import Snoowrap from 'snoowrap'
 
 const r = new Snoowrap({
-  userAgent: "winnin-desafio-frontend",
-  clientId: "WikUjYybBABEhQ",
-  refreshToken: "135720659019-PfdjvJYLwtP5rIzbdiYC0VfIgFc",
-  accessToken: "135720659019-XW0GC0bqaImr261cS6aEsFb2KDY"
-});
+  userAgent: process.env.USER_AGENT,
+  clientId: process.env.CLIENT_ID,
+  refreshToken: process.env.REFRESH_TOKEN,
+  accessToken: process.env.ACCESS_TOKEN,
+})
 
-export default r;
+export default r
