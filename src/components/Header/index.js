5f33a39cd8ed75cafa10a8bf874db9cd91871c73
Felipe Gonçalves <felipeog476@gmail.com>
2020-01-23 18:01:59 -0300

    Add useTheme

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,43 @@
-import { Navigation } from 'components'
-
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
+import { useTheme } from 'emotion-theming'
+import { Navigation } from 'components'
 
-const Header = () => (
-  <header
-    css={(theme) => css`
-      background-color: ${theme.header.background};
-      position: fixed;
-      top: 0;
-      width: 100%;
-    `}
-  >
-    <h1
-      css={(theme) => css`
-        font-size: 1.6rem;
-        color: ${theme.header.text};
-        font-weight: bold;
-        text-align: center;
-        text-transform: uppercase;
-        padding: 1.6rem;
+const Header = () => {
+  const theme = useTheme()
+
+  return (
+    <header
+      css={css`
+        background-color: ${theme.header.background};
+        position: fixed;
+        top: 0;
+        width: 100%;
       `}
     >
-      React
-      <span
-        css={(theme) => css`
-          color: ${theme.color.accent};
+      <h1
+        css={css`
+          font-size: 1.6rem;
+          color: ${theme.header.text};
+          font-weight: bold;
+          text-align: center;
+          text-transform: uppercase;
+          padding: 1.6rem;
         `}
       >
-        JS
-      </span>
-    </h1>
+        React
+        <span
+          css={css`
+            color: ${theme.color.accent};
+          `}
+        >
+          JS
+        </span>
+      </h1>
 
-    <Navigation />
-  </header>
-)
+      <Navigation />
+    </header>
+  )
+}
 
 export default Header
