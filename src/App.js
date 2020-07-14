6827b0a0060bec4505206b62dfda31818384c6f4
Felipe <felipe@MacBook-Pro-de-Felipe.local>
2020-07-13 23:22:52 -0300

    Improvements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,28 +7,24 @@ import {
 } from 'react-router-dom'
 import { Header, HotFeed, NewFeed, RisingFeed } from 'components'
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Header />
+const App = () => (
+  <Router>
+    <Header />
 
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/hot" />
-          </Route>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/hot" />
+      </Route>
 
-          <Route path="/hot" component={HotFeed} />
-          <Route path="/new" component={NewFeed} />
-          <Route path="/rising" component={RisingFeed} />
+      <Route path="/hot" component={HotFeed} />
+      <Route path="/new" component={NewFeed} />
+      <Route path="/rising" component={RisingFeed} />
 
-          <Route>
-            <Redirect to="/hot" />
-          </Route>
-        </Switch>
-      </Router>
-    )
-  }
-}
+      <Route>
+        <Redirect to="/hot" />
+      </Route>
+    </Switch>
+  </Router>
+)
 
 export default App
