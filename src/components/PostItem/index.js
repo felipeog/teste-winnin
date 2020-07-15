e1c9509294a4c1e91e78aac14aabef885aef0ea8
Felipe <felipe@MacBook-Pro-de-Felipe.local>
2020-07-14 21:14:11 -0300

    Add upvote ratio

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -3,9 +3,12 @@ import moment from 'moment'
 import 'moment/locale/pt-br'
 import './index.scss'
 import Pin from '../../assets/img/pin.svg'
+import ArrowUp from '../../assets/img/arrowUp.svg'
 
 const PostItem = ({ post }) => {
-  const { title, author, stickied, url } = post
+  const { title, author, stickied } = post
+  const upvoteRatio = post.upvote_ratio * 100
+  const url = `https://www.reddit.com/${post.permalink}`
   const thumb = post.thumbnail.match('http') ? post.thumbnail : null
   const time = moment.unix(post.created_utc).fromNow()
 
@@ -19,13 +22,18 @@ const PostItem = ({ post }) => {
       <article className="post-item">
         {thumb && <img className="post-item__thumb" src={thumb} alt={title} />}
 
-        <div className="post-item__content">
-          <h1 className="post-item__title">{title}</h1>
-          <p className="post-item__meta">
-            <span className="post-item__time">Enviado {time}</span> por{' '}
-            <span className="post-item__author">{author}</span>
+        <div className="content">
+          <h1 className="content__title">{title}</h1>
+          <p className="content__meta">
+            <span className="content__time">Enviado {time}</span> por{' '}
+            <span className="content__author">{author}</span>
           </p>
-          <p className="post-item__url">{url}</p>
+          <p className="content__url">{url}</p>
+        </div>
+
+        <div className="upvote-ratio">
+          <img className="upvote-ratio__icon" src={ArrowUp} alt="" />
+          <p className="upvote-ratio__count">{upvoteRatio}%</p>
         </div>
 
         {stickied && <img className="post-item__pin" src={Pin} alt="" />}
